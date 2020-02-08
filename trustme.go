ac6858cc4d1f895d6e0c03812fcb697f7a87bccd
Luboš Pokorný <zoido@users.noreply.github.com>
2020-02-08 17:46:08 +0100

    use LeafCert internaly for CA (#5)

--- trustme.go
+++ trustme.go
@@ -1,8 +1,27 @@
 package trustme
 
-import "github.com/zoido/trustme-go/ca"
+import (
+	"testing"
 
-// NewCA returns new instance of th CA.
-func NewCA(options ...ca.Option) (*ca.CA, error) {
-	return ca.New(options...)
+	"github.com/zoido/trustme-go/ca"
+)
+
+// CA is a fake certification authority for issuing TLS certificates for tests.
+type CA struct {
+	ca      *ca.CA
+	testing *testing.T
+}
+
+// MustCreateNewCA returns new instance of th CA and fails the test when creation fails.
+func MustCreateNewCA(t *testing.T, options ...ca.Option) *CA {
+	c, err := ca.New(options...)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	return &CA{
+		ca:      c,
+		testing: t,
+	}
 }
