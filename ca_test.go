1df598c0d55ad6e9f48b8b06b669cd58df338f28
Luboš Pokorný <zoido@users.noreply.github.com>
2020-02-09 15:02:28 +0100

    SANs options for certificates. (#15)

--- ca_test.go
+++ ca_test.go
@@ -3,12 +3,15 @@ package trustme_test
 import (
 	"crypto/x509"
 	"math/big"
+	"net"
+	"net/url"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/zoido/trustme-go"
 	"github.com/zoido/trustme-go/ca"
+	"github.com/zoido/trustme-go/cert"
 )
 
 type CATestSuite struct {
@@ -32,11 +35,11 @@ func (s *CATestSuite) TestCA_CertProperties() {
 	fca := trustme.New(s.T())
 
 	// Then
-	s.Require().True(fca.Certificate().IsCA, "Certificate should be CA")
+	s.Require().True(fca.Certificate().IsCA, "CA certificate should be CA")
 	s.Require().Equal(
 		x509.KeyUsageKeyEncipherment|x509.KeyUsageDigitalSignature|x509.KeyUsageCertSign,
 		fca.Certificate().KeyUsage,
-		"Certificate should have proper usage set",
+		"CA certificate should have proper usage set",
 	)
 }
 
@@ -80,7 +83,7 @@ func (s *CATestSuite) TestCA_WithRSABits_Effective() {
 	fca := trustme.New(s.T(), ca.WithRSABits(512))
 
 	// Then
-	s.Require().Equal(512/8, fca.Key().Size(), "Key should have size set via options")
+	s.Require().Equal(512/8, fca.Key().Size(), "CA key should have size set via options")
 }
 
 func (s *CATestSuite) TestCA_WithCommonName_Effective() {
@@ -118,16 +121,16 @@ func (s *CATestSuite) TestCA_MultipleOptions_Effective() {
 	// Then
 	s.Require().Equal(
 		"Trust Me, Org.", fca.Certificate().Subject.Organization[0],
-		"Certificate should have O set via options",
+		"CA certificate should have O set via options",
 	)
 	s.Require().Equal(
 		"test-CN", fca.Certificate().Subject.CommonName,
-		"Certificate should have CN set via options",
+		"CA certificate should have CN set via options",
 	)
-	s.Require().Equal(1024/8, fca.Key().Size(), "Key should have size set via options")
+	s.Require().Equal(1024/8, fca.Key().Size(), "CA key should have size set via options")
 	s.Require().Equal(
 		time.Minute*123, fca.Certificate().NotAfter.Sub(fca.Certificate().NotBefore),
-		"Certificate should have TTL set via CA options",
+		"CA certificate should have TTL set via CA options",
 	)
 }
 
@@ -142,10 +145,10 @@ func (s *CATestSuite) TestCA_WithOptions_Effective() {
 	)
 
 	// Then
-	s.Require().Equal(1024/8, fca.Key().Size(), "Key should have size set via options")
+	s.Require().Equal(1024/8, fca.Key().Size(), "CA key should have size set via options")
 	s.Require().Equal(
 		time.Minute*123, fca.Certificate().NotAfter.Sub(fca.Certificate().NotBefore),
-		"Certificate should have TTL set via CA options",
+		"CA certificate should have TTL set via CA options",
 	)
 }
 
@@ -273,9 +276,171 @@ func (s *CATestSuite) TestCA_Issue_WithOptionsDefalts_Effective() {
 	crt := fca.MustIssue()
 
 	// Then
+	s.Require().Equal(1024/8, crt.Key.Size(), "Key should have size set via options")
 	s.Require().Equal(
 		time.Minute*123, crt.Certificate.NotAfter.Sub(crt.Certificate.NotBefore),
 		"Certificate should have TTL set via CA options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithTTL_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(cert.WithTTL(time.Hour * 123))
+
+	// Then
+	s.Require().Equal(
+		time.Hour*123, crt.Certificate.NotAfter.Sub(crt.Certificate.NotBefore),
+		"Certificate should have TTL set via options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithRSABits_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(cert.WithRSABits(1024))
+
+	// Then
+	s.Require().Equal(1024/8, crt.Key.Size(), "Key should have size set via options")
+}
+
+func (s *CATestSuite) TestCA_Issue_WithCommonName_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(cert.WithCommonName("cert-test-CN"))
+
+	// Then
+	s.Require().Equal(
+		"cert-test-CN", crt.Certificate.Subject.CommonName,
+		"Certificate should have CN set via options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_MultipleOptions_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithRSABits(1024),
+		cert.WithTTL(time.Minute*123),
+	)
+
+	// Then
+	s.Require().Equal(1024/8, crt.Key.Size(), "Key should have size set via options")
+	s.Require().Equal(
+		time.Minute*123, crt.Certificate.NotAfter.Sub(crt.Certificate.NotBefore),
+		"Certificate should have TTL set via CA options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithOptions_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithOptions(
+			cert.WithRSABits(1024),
+			cert.WithTTL(time.Minute*123),
+		),
+	)
+
+	// Then
+	s.Require().Equal(1024/8, crt.Key.Size(), "Key should have size set via options")
+	s.Require().Equal(
+		time.Minute*123, crt.Certificate.NotAfter.Sub(crt.Certificate.NotBefore),
+		"Certificate should have TTL set via CA options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithDNS_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithDNS("dns.a"),
+		cert.WithDNS("dns.b"),
+	)
+
+	// Then
+	s.Require().ElementsMatch(
+		crt.Certificate.DNSNames, []string{"dns.a", "dns.b"},
+		"Certificate should have DNS names SAN set via options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithIP_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithIP(net.IPv6loopback),
+		cert.WithIP(net.IPv4(8, 8, 8, 8)),
+		cert.WithIP(net.IPv4(127, 0, 0, 1)),
+	)
+
+	// Then
+	certIPs := make([]string, 0, len(crt.Certificate.IPAddresses))
+	for _, ip := range crt.Certificate.IPAddresses {
+		certIPs = append(certIPs, ip.String())
+	}
+	s.Require().ElementsMatch(
+		certIPs, []string{"8.8.8.8", "127.0.0.1", "::1"},
+		"Certificate should have IP addresses names SAN set via options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithEmail_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithEmail("example@example.com"),
+		cert.WithEmail("test@example.com"),
+	)
+
+	// Then
+	s.Require().ElementsMatch(
+		crt.Certificate.EmailAddresses, []string{"example@example.com", "test@example.com"},
+		"Certificate should have email addresses names SAN set via options",
+	)
+}
+
+func (s *CATestSuite) TestCA_Issue_WithURI_Effective() {
+	// Given
+	fca := trustme.New(s.T())
+
+	uri1 := &url.URL{
+		Scheme: "https",
+		Host:   "example.com",
+		Path:   "/test/path",
+	}
+	uri2 := &url.URL{
+		Scheme: "spiffe",
+		Host:   "trust.domain",
+		Path:   "/test/path",
+	}
+
+	// When
+	crt := fca.MustIssue(
+		cert.WithURI(uri1),
+		cert.WithURI(uri2),
+	)
+
+	// Then
+	s.Require().ElementsMatch(
+		crt.Certificate.URIs, []*url.URL{uri1, uri2},
+		"Certificate should have email addresses names SAN set via options",
 	)
 }
 
--- cert/leafcert.go
+++ cert/leafcert.go
@@ -3,6 +3,8 @@ package cert
 import (
 	"crypto/rsa"
 	"crypto/x509"
+	"net"
+	"net/url"
 	"time"
 )
 
@@ -22,16 +24,70 @@ type Config struct {
 
 	CommonName   string
 	Organization string
+
+	DNSNames       []string
+	EmailAddresses []string
+	IPAddresses    []net.IP
+	URIs           []*url.URL
 }
 
 // Option configures the issued certificate.
 type Option func(cfg *Config)
 
-// WithOptions turns a list of LeafOption instances into an LeafOption.
+// WithOptions turns a list of Option instances into an Option.
 func WithOptions(opts ...Option) Option {
 	return func(cfg *Config) {
 		for _, opt := range opts {
 			opt(cfg)
 		}
+	}
+}
+
+// WithTTL configures time to live of the issued certificate.
+func WithTTL(ttl time.Duration) Option {
+	return func(cfg *Config) {
+		cfg.TTL = ttl
+	}
+}
+
+// WithRSABits configures the length of RSA private key issued certificate.
+func WithRSABits(rsaBits int) Option {
+	return func(cfg *Config) {
+		cfg.RSABits = rsaBits
+	}
+}
+
+// WithCommonName configures common name of the issued certificate.
+func WithCommonName(commonName string) Option {
+	return func(cfg *Config) {
+		cfg.CommonName = commonName
+	}
+}
+
+// WithDNS configures DNS names SANs of the issued certificate. Can be used multiple times.
+func WithDNS(name string) Option {
+	return func(cfg *Config) {
+		cfg.DNSNames = append(cfg.DNSNames, name)
+	}
+}
+
+// WithIP configures DNS names SANs of the issued certificate. Can be used multiple times.
+func WithIP(ipAddress net.IP) Option {
+	return func(cfg *Config) {
+		cfg.IPAddresses = append(cfg.IPAddresses, ipAddress)
+	}
+}
+
+// WithEmail configures e-mail adresses SANs of the issued certificate. Can be used multiple times.
+func WithEmail(email string) Option {
+	return func(cfg *Config) {
+		cfg.EmailAddresses = append(cfg.EmailAddresses, email)
+	}
+}
+
+// WithURI configures URIs SANs of the issued certificate. Can be used multiple times.
+func WithURI(uri *url.URL) Option {
+	return func(cfg *Config) {
+		cfg.URIs = append(cfg.URIs, uri)
 	}
 }
