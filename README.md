0671082446a13cd682a6ab21837aee005e131e02
Luboš Pokorný <lubos.pokorny@gmail.com>
2020-02-16 20:30:47 +0100

    Example Test

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 # Trust me. Go!
 <!-- markdownlint-enable MD026 -->
 
-![Go](https://github.com/zoido/trustme-go/workflows/Go/badge.svg)
+[![Go](https://github.com/zoido/trustme-go/workflows/Go/badge.svg)](https://github.com/zoido/trustme-go/actions?query=workflow%3AGo)
 [![codecov](https://codecov.io/gh/zoido/trustme-go/branch/master/graph/badge.svg)](https://codecov.io/gh/zoido/trustme-go)
 [![Go Report Card](https://goreportcard.com/badge/github.com/zoido/trustme-go)](https://goreportcard.com/report/github.com/zoido/trustme-go)
 
-Inspired by [trustme](https://github.com/python-trio/trustme) for [Python](https://www.python.org/).
+Inspired by [trustme](https://github.com/python-trio/trustme)
+for [Python](https://www.python.org/).
 
 `trustme-go` is a small Go package that offers you with fake
-[certificate autority](https://en.wikipedia.org/wiki/Certificate_authority) (CA)
-that issues TLS certificates for Go tests.
+[certificate autority](https://en.wikipedia.org/wiki/Certificate_authority)
+(CA) that issues TLS certificates for Go tests for the cases when
+[`httptest.NewTLSServer`](https://golang.org/pkg/net/http/httptest/#NewTLSServer)
+is not enough.
---
+++ example_test.go
@@ -0,0 +1,67 @@
+package trustme_test
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/zoido/trustme-go"
+)
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	if len(r.TLS.PeerCertificates) == 0 {
+		http.Error(w, "Requires mTLS", http.StatusUnauthorized)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(r.TLS.PeerCertificates[0].Subject.CommonName))
+}
+
+func TestExample(t *testing.T) {
+	ca := trustme.New(t)
+	serverCert := ca.MustIssue(trustme.WithIP(net.ParseIP("127.0.0.1")))
+	clientCert := ca.MustIssue(trustme.WithCommonName("TEST CLIENT"))
+
+	serverConfig := serverCert.AsServerConfig()
+	serverConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	listener, err := tls.Listen("tcp", "127.0.0.1:0", serverConfig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer listener.Close()
+
+	srv := http.Server{
+		Handler: http.HandlerFunc(ExampleHandler),
+	}
+	defer srv.Close()
+	go srv.Serve(listener)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: clientCert.AsClientConfig(),
+		},
+		Timeout: time.Second * 5,
+	}
+
+	resp, err := client.Get(fmt.Sprintf("https://%s/", listener.Addr().String()))
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Server did not return OK status")
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(b, []byte("TEST CLIENT")) != 0 {
+		t.Fatal("Server did not return expected peer CN")
+	}
+}
